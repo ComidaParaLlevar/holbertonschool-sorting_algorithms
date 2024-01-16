6ccc6d91930e5deff7e376b7b16679d48a44e650
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-16 16:47:45 -0400

    added recursive quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -14,3 +14,21 @@ void quick_sort(int *array, size_t size)
 
 	quick_sort_recursive(array, 0, size - 1, size);
 }
+
+/**
+ * quick_sort_recursive - Recursively sorts a partition of an array using
+ *                       the Quick sort algorithm with the Lomuto partition scheme.
+ * @array: The array to be sorted.
+ * @low: The starting index of the partition.
+ * @high: The ending index of the partition.
+ * @size: The size of the array.
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot_index = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot_index - 1, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
