40b3674f80041efc031c1b246a5a8a1b61c51b0d
comidaparallevar <lyanosorio98@gmail.com>
2024-01-17 13:15:27 -0400

    added 0-bubble_sort to branch lyan

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array in ascending order
+ * @array: pointer to array
+ * @size: size of array
+ * Return: nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp = 0, is_swapped;
+
+    if (array == NULL)
+        return;
+    
+    for (i = 0; i < size - 1; i++)
+        {
+            is_swapped = 0;
+            for (j = 0; j < size - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                        {
+                            temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            print_array(array, size);
+                            is_swapped = 1;
+                        }
+                }
+            if (is_swapped == 0)
+                break;
+        }
+}
