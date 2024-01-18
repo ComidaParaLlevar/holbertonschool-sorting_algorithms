4ce746cfac5cefd923b24da3a3552e19f136e56d
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-18 16:14:15 -0400

    added functions to header

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "sort.h"
 
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+
 /**
  * quick_sort - Sorts an array of integers in ascending order using
  *              the Quick sort algorithm with the Lomuto partition scheme.
@@ -9,10 +12,10 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2)
-		return;
+    if (size < 2)
+        return;
 
-	quick_sort_recursive(array, 0, size - 1, size);
+    quick_sort_recursive(array, 0, size - 1, size);
 }
 
 /**
@@ -25,12 +28,12 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-	if (low < high)
-	{
-		int pivot_index = lomuto_partition(array, low, high, size);
-		quick_sort_recursive(array, low, pivot_index - 1, size);
-		quick_sort_recursive(array, pivot_index + 1, high, size);
-	}
+    if (low < high)
+    {
+        int pivot_index = lomuto_partition(array, low, high, size);
+        quick_sort_recursive(array, low, pivot_index - 1, size);
+        quick_sort_recursive(array, pivot_index + 1, high, size);
+    }
 }
 
 /**
@@ -44,26 +47,27 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-		int pivot = array[high];
-		int i = low - 1;
-		int j, temp;
+    int pivot = array[high];
+    int i = low - 1;
+    int j, temp;
 
-		for (j = low; j <= high - 1; j++)
-		{
-			if (array[j] < pivot)
-			{
-				i++;
-				temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				print_array(array, size);
-			}
-		}
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            print_array(array, size);
+        }
+    }
 
-		temp = array[i + 1];
-		array[i + 1] = array[high];
-		array[high] = temp;
-		print_array(array, size);
+    temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+    print_array(array, size);
 
-		return (i + 1);
+    return (i + 1);
 }
+
--- sort.h
+++ sort.h
@@ -21,5 +21,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 #endif
