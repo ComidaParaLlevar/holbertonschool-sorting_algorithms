54ec93b4c321ee022b31109f6f9c2bf2c011470e
comidaparallevar <lyanosorio98@gmail.com>
2024-01-18 10:23:30 -0400

    merge fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,31 +7,6 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-<<<<<<< HEAD
-    size_t i, j;
-    int temp = 0, is_swapped;
-
-    if (array == NULL)
-        return;
-    
-    for (i = 0; i < size - 1; i++)
-        {
-            is_swapped = 0;
-            for (j = 0; j < size - 1; j++)
-                {
-                    if (array[j] > array[j + 1])
-                        {
-                            temp = array[j];
-                            array[j] = array[j + 1];
-                            array[j + 1] = temp;
-                            print_array(array, size);
-                            is_swapped = 1;
-                        }
-                }
-            if (is_swapped == 0)
-                break;
-        }
-=======
 	size_t i, j;
 	int temp = 0, is_swapped;
 
@@ -55,5 +30,4 @@ void bubble_sort(int *array, size_t size)
 	if (is_swapped == 0)
 		break;
 	}
->>>>>>> ce38456cfe3a4347f4147992627f1d98f74c57b0
 }
