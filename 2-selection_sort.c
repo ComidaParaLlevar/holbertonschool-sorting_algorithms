d50d4cfdd471b753b0d1ea6b5677ea53368cb53c
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-01-19 08:55:36 -0400

    fixed betty code error in 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,17 +10,14 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
-
 	for (i = 0; i < size - 1; i++)
 	{
 	min_idx = i;
-
 	for (j = i + 1; j < size; j++)
 	{
 		if (array[j] < array[min_idx])
 		min_idx = j;
 	}
-
 	if (min_idx != i)
 		{
 		int temp = array[i];
